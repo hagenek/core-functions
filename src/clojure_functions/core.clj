6616d4290cfb358789480b593c50a0bdf8932b47
Georg Ekeberg <georghagen@gmail.com>
2021-01-28 15:24:47 +0100

    Made a filter function

--- src/clojure_functions/core.clj
+++ src/clojure_functions/core.clj
@@ -1,20 +1,46 @@
 (ns clojure-functions.core)
-(use 'clojure.repl)
 
-;;; This is an incorrect implementation, such as might be written by
-;;; someone who was used to a Lisp in which an empty list is equal to
-;;; nil.
-(defn first-element [sequence default]
-  (if (empty? sequence)
-    default
-    (first sequence)))
 
 (defn my-reduce
   "expects a funtion a value and a collection"
-  ([f coll] (my-reduce f (f (first coll) (second coll)) coll))
+  ([f coll]
+   (if (empty? coll) (f)
+       (my-reduce f (f (first coll) (second coll)) (rest (rest coll)))))
   ([f val coll]
    (loop [acc val remaining-coll coll]
      (if (empty? remaining-coll)
        acc
        (recur  (f acc (first remaining-coll)) (rest remaining-coll))))))
 
+(defn my-count
+  "returns the number of items in a collection"
+  ([coll] (my-count coll 0))
+  ([coll acc]
+   (if (empty? coll)
+     acc
+     (my-count (rest coll) (inc acc)))))
+
+
+(defn return-one [n] (+ (- n n) 1))
+
+(defn my-count2
+  "refactored my-count"
+  [coll]
+  (reduce + (map return-one coll)))
+
+(defn counting-helper [acc _] (inc acc))
+
+(defn my-count3 [coll]
+  (my-reduce counting-helper 0 coll))
+
+(my-count3 [1 3 6 5 7])
+
+(defn my-filter
+  "(filter pred coll) Pred must be free of side effects."
+  [pred coll]
+  (loop [remaining-coll coll new-coll []]
+    (if (empty? remaining-coll)
+      (lazy-seq new-coll)
+      (if (pred (first remaining-coll))
+        (recur (rest remaining-coll) (conj new-coll (first remaining-coll)))
+        (recur (rest remaining-coll) new-coll)))))
--- test/clojure_functions/core_test.clj
+++ test/clojure_functions/core_test.clj
@@ -10,7 +10,33 @@
        (fact "result 2 for add function with val 1 and coll with 1 in it"
              (my-reduce + 1 [1]) => 2)
        (fact "result 6 for add function, val of 0 and a collection of 1 2 3 in it"
-             (my-reduce + 0 [1 2 3]) => 6))
+             (my-reduce + 0 [1 2 3]) => 6)
+       (fact "return result of calling function on an empty collection"
+             (my-reduce (fn [] "yes") []) => "yes")
+       (fact "uses intial value of function applied to first argu, and second argument
+              if no start val is supplied"
+             (my-reduce + [1 2 3]) => 6))
+
+(facts "about my own count function"
+       (fact "result 0 for an empty list"
+             (my-count3 '()) => 0)
+       (fact "result 0 for nil"
+             (my-count3 nil) => 0) 4
+       (fact "result 1 for a list of one item"
+             (my-count3 '(1)) => 1)
+       (fact "result 2 for a list of two items"
+             (my-count3 '(1 2)) => 2)
+       (fact "result 10 for a list of 10 items"
+             (my-count3 [1 2 3 4 5 6 7 8 9 10]) => 10)
+       (fact "work with maps, result 2 with 2 key value pairs"
+             (my-count3 {:key1 "one" :key2 "two"}) => 2))
+
+(facts "about my own implementation of filter"
+       (fact "Results in empty lazy seq. when filtering for zero in an empty vector"
+             (class (my-filter zero? [])) => clojure.lang.LazySeq
+             (my-count (my-filter zero? [])) => 0))
+(fact "results in an lazy sequence with one element with filtering for even in a list when one even number"
+      (my-filter even? [1 5 3 2]) => (lazy-seq [2]))
 
 (facts "about `first-element`"
        (fact "it normally returns the first element"
